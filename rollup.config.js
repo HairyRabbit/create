c63c26e8319e48a04295d9ddfb1b5103f5431067
yuffiy <yfhj1990@hotmail.com>
2018-02-09 11:34:53 +0800

    update.

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,6 @@ import resolve from 'rollup-plugin-node-resolve'
 import json from 'rollup-plugin-json'
 import commonjs from 'rollup-plugin-commonjs'
 import babel from 'rollup-plugin-babel'
-import uglify from 'rollup-plugin-uglify-es'
 import pkg from './package.json'
 
 export default {
@@ -18,7 +17,6 @@ export default {
     babel({ exclude: 'node_modules/**' }),
     json({ exclude: 'node_modules/**' }),
     commonjs({ exclude: 'node_modules/**' }),
-    'production' === process.env.NODE_ENV ? uglify({}) : []
   ),
   external: Object.keys(pkg.dependencies)
 }
